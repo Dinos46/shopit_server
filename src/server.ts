a8d146eea2832f31164feda0eb0a218aa6ad2d5c
Din <dinos4620@gmail.com>
2022-02-20 18:17:49 +0200

    test db

--- src/server.ts
+++ src/server.ts
@@ -8,15 +8,15 @@ import { rootResolvers } from './graphql/resolvers/rootResolvers';
 const app = express();
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')));
-} else {
-  const corsOptions = {
-    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.resolve(__dirname, 'public')));
+// } else {
+//   const corsOptions = {
+//     origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+//     credentials: true,
+//   };
+// }
+app.use(cors());
 
 // type httpType = {
 //   req: express.Request;
