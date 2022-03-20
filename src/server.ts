c4e4ec467ebf71526d0d08c1de2863a3ab36230b
unknown <dinos4620@gmail.com>
2022-03-20 17:58:45 +0200

    clear logs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import cors from "cors";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import { rootSchema } from "./graphql/shcema/rootSchema";
 import { itemResolvers } from "./graphql/resolvers/itemResolvers";
 import { authResolvers } from "./graphql/resolvers/authResolvers";
-import { rootSchema } from "./graphql/shcema/rootSchema";
 
 const app = express();
 app.use(express.json());
