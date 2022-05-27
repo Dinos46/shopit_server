3bfd147aac3d1e1f63c1728edf8085d743ab8a88
unknown <dinos4620@gmail.com>
2022-05-28 00:03:34 +0300

    fix try

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,6 +1,6 @@
 import { getAuth } from "firebase-admin/auth";
 import { initializeApp } from "firebase-admin/app";
-import { credential, ServiceAccount } from "firebase-admin";
+import { credential } from "firebase-admin";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -15,7 +15,7 @@ dotenv.config();
 //   auth_provider_x509_cert_url: process.env.FB_TYPE_AUTH_PROVIDER_X509_CERT_UR,
 //   client_x509_cert_url: process.env.FB_TYPE_CLIENT_X509_CERT_URL,
 // } as ServiceAccount;
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("../../config/firebaseConfig.json");
 
 const app = initializeApp({
   credential: credential.cert(serviceAccount),
