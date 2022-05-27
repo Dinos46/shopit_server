d2dfcd75de0b31a6c81e97afd281ce3e948568e6
unknown <dinos4620@gmail.com>
2022-05-28 00:08:39 +0300

    fix try

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,24 +1,25 @@
 import { getAuth } from "firebase-admin/auth";
 import { initializeApp } from "firebase-admin/app";
-import { credential } from "firebase-admin";
+import { credential, ServiceAccount } from "firebase-admin";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const firebaseCreds = {
-//   type: process.env.FB_TYPE,
-//   project_id: process.env.FB_PROJECT_ID,
-//   private_key: process.env.FB_TYPE_PRIVATE_KEY,
-//   client_email: process.env.FB_TYPE_CKIENT_EMAIL,
-//   client_id: process.env.FB_TYPE_CKIENT_ID,
-//   auth_uri: process.env.FB_TYPE_AUTH_URI,
-//   token_uri: process.env.FB_TYPE_TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.FB_TYPE_AUTH_PROVIDER_X509_CERT_UR,
-//   client_x509_cert_url: process.env.FB_TYPE_CLIENT_X509_CERT_URL,
-// } as ServiceAccount;
-var serviceAccount = require("../../config/firebaseConfig.json");
+const firebaseCreds = {
+  type: process.env.FB_TYPE,
+  project_id: process.env.FB_PROJECT_ID,
+  private_key: process.env.FB_TYPE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FB_TYPE_CKIENT_EMAIL,
+  client_id: process.env.FB_TYPE_CKIENT_ID,
+  auth_uri: process.env.FB_TYPE_AUTH_URI,
+  token_uri: process.env.FB_TYPE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_TYPE_AUTH_PROVIDER_X509_CERT_UR,
+  client_x509_cert_url: process.env.FB_TYPE_CLIENT_X509_CERT_URL,
+} as ServiceAccount;
+// var serviceAccount = require("../../config/firebaseConfig.json");
+// privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 
 const app = initializeApp({
-  credential: credential.cert(serviceAccount),
+  credential: credential.cert(firebaseCreds),
 });
 
 export const auth = getAuth(app);
